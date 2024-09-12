1e65669d8ac9b75130519d4b5e6f0cc01940ae84
Krishna Thakkar <work.krishnat@gmail.com>
2024-09-12 21:27:10 +0530

    companion frontend message schema

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import Categories from "@/components/Categories";
+import Companions from "@/components/Companions";
 import SearchInput from "@/components/SearchInput";
 import prismadb from "@/lib/db";
 import { UserButton } from "@clerk/nextjs";
@@ -6,13 +7,41 @@ import React from "react";
 
 type Props = {};
 
-const page = async (props: Props) => {
-  const categories = (await prismadb.category.findMany()) || [];
+interface PageProps {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  };
+}
+
+const page = async ({ searchParams }: PageProps) => {
+  const data = await prismadb.companion.findMany({
+    relationLoadStrategy: "join",
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name,
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  });
+
+  const categories = await prismadb.category.findMany();
 
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
       <Categories categories={categories} />
+      <Companions data={data} />
     </div>
   );
 };
